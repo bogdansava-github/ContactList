9f08b1f2ddc1de73137c7fe72ac1847a8689e860
bogdansava-github <bogdan.sava@me.com>
2020-03-03 21:52:05 +0200

    implemented DBUserService

--- src/main/java/ro/jademy/contactlist/Main.java
+++ src/main/java/ro/jademy/contactlist/Main.java
@@ -1,15 +1,33 @@
 package ro.jademy.contactlist;
 
+import ro.jademy.contactlist.service.DBUserService;
 import ro.jademy.contactlist.service.FileUserService;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.SQLException;
 import java.util.*;
 
 public class Main {
     public static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        Menu menu = new Menu(new FileUserService("contacts.csv"));
+
+        //DBUserService db=new DBUserService(DBUserService.getConnection(getProperties()));
+
+        //Menu menu = new Menu(new FileUserService("contacts.csv"));
+        Menu menu = new Menu(new DBUserService(DBUserService.getConnection(getProperties())));
         menu.showMenu(new UserForm());
+    }
+
+    public static Properties getProperties() {
+        Properties prop = new Properties();
+        try (InputStream input = DBUserService.class.getResourceAsStream("/application.properties")){
+            prop.load(input);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return prop;
     }
 }
 
--- src/main/java/ro/jademy/contactlist/service/DBUserService.java
+++ src/main/java/ro/jademy/contactlist/service/DBUserService.java
@@ -1,21 +1,86 @@
 package ro.jademy.contactlist.service;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.Reader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Properties;
 
-public class DBUserService {
+import org.apache.ibatis.jdbc.ScriptRunner;
 
 
-    public static Connection getConnection() throws SQLException {
-        Properties connectionProps=new Properties();
-        connectionProps.put("user", "root");
-        connectionProps.put("password","@@vipermatrix");
+public class DBUserService implements UserService {
 
-        Connection conn = DriverManager.getConnection("jdbc:"+"mysql"+"://"+
-                "localhost"+":"+"3306"+"/employees",connectionProps);
+    private Connection conn;
 
-        return conn;
+    public DBUserService(Connection conn) {
+        this.conn = conn;
+    }
+
+    public static Properties setConnectionProps(Properties props) {
+
+        Properties connectionProps = new Properties();
+        connectionProps.put("user", props.getProperty("db.user"));
+        connectionProps.put("password", props.getProperty("db.password"));
+        return connectionProps;
+    }
+
+    //Open a connection
+    public static Connection getConnection(Properties props) {
+
+        String dbUrl = "jdbc:mysql://" + props.getProperty("db.url") + ":" + props.getProperty("db.port") + "/";
+        String dbName = props.getProperty("db.name");
+        try {
+            Connection conn = DriverManager.getConnection(dbUrl + dbName, setConnectionProps(props));
+            return conn;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        public void createDataBase (Properties props){
+            Statement stmt = null;
+            String dbUrl = "jdbc:mysql://" + props.getProperty("db.url") + ":" + props.getProperty("db.port") + "/";
+            String dbName = props.getProperty("db.name");
+            try {
+                //Create Database if not exists
+                System.out.println("Connecting ...");
+                Connection conn = DriverManager.getConnection(dbUrl, setConnectionProps(props));
+                System.out.println("Connection established!");
+
+                System.out.println("Creating database...");
+                stmt = conn.createStatement();
+                stmt.execute("CREATE DATABASE " + dbName);
+                System.out.println("Database created!");
+                System.out.println("Creating tables......");
+
+
+                //Create tables from a SQL Script
+
+                //Initialize the script runner
+                ScriptRunner sr = new ScriptRunner(conn);
+                //Creating a reader object
+                Reader reader = new BufferedReader(new FileReader("contactList.sql"));
+                //Running the script
+                sr.runScript(reader);
+                System.out.println("Tables created!");
+
+                //insert contacts in dbase
+                reader = new BufferedReader(new FileReader("contactsInsert.sql"));
+                sr.runScript(reader);
+
+            } catch (SQLException e) {
+                System.out.println("Database exist!");
+                System.out.println("Accessing " + dbName + " database ...\n");
+                //stmt.execute("USE " + dbName);
+                //System.exit(0);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+
+        }
     }
 }
