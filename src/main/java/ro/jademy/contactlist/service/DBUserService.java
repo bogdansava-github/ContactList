56c58fe858865ba9f12db9c1da07cd610896f530
bogdansava-github <bogdan.sava@me.com>
2020-03-04 22:28:05 +0200

    db service

--- src/main/java/ro/jademy/contactlist/service/DBUserService.java
+++ src/main/java/ro/jademy/contactlist/service/DBUserService.java
@@ -4,10 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.Reader;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.Properties;
 
 import org.apache.ibatis.jdbc.ScriptRunner;
